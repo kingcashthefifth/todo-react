2f1cff0c63eb33aa976544ecb8229173b9fb2e8a
kingcashthefifth <kingcashthefifth@gmail.com>
2019-02-17 21:53:57 +0800

    completed most furthers (1,2,3,5,6,last), left edit and exire not done)

--- script.jsx
+++ script.jsx
@@ -17,7 +17,6 @@ class ToDoApp extends React.Component {
   render() {
     return (
       <div>
-        {/* <NewList /> */}
         <ListArea />
       </div>
     );
@@ -73,10 +72,13 @@ class List extends React.Component {
     super();
     this.todoWordChecker = this.todoWordChecker.bind(this);
     this.createNewTodo = this.createNewTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.doneTodo = this.doneTodo.bind(this);
   }
 
   state = {
     todo: [],
+    done: [],
     word: ""
   };
 
@@ -92,10 +94,47 @@ class List extends React.Component {
     }
   }
 
+  deleteTodo(description, detectDone) {
+    if (detectDone) {
+      let arr = [...this.state.done];
+      arr.splice(arr.indexOf(description), 1);
+      this.setState({ done: arr });
+    } else {
+      let arr = [...this.state.todo];
+      arr.splice(arr.indexOf(description), 1);
+      this.setState({ todo: arr });
+    }
+  }
+
+  doneTodo(description) {
+    this.deleteTodo(description);
+    let arr = [...this.state.done, description];
+    this.setState({ done: arr });
+  }
+
   render() {
     const todoItems = this.state.todo.map((todo, index) => {
-      return <TodoItem key={index} description={todo} />;
+      return (
+        <TodoItem
+          key={index}
+          description={todo}
+          deleteTodo={this.deleteTodo}
+          doneTodo={this.doneTodo}
+        />
+      );
     });
+
+    const doneItems = this.state.done.map((done, index) => {
+      return (
+        <TodoItem
+          key={index}
+          donedes={done}
+          deleteTodo={this.deleteTodo}
+          detectDone={true}
+        />
+      );
+    });
+
     return (
       <div className="list">
         <h4>
@@ -104,6 +143,7 @@ class List extends React.Component {
         <input onChange={this.todoWordChecker} value={this.state.word} />
         <button onClick={this.createNewTodo}>New Todo</button>
         {todoItems}
+        {doneItems}
         {/* <h1>{this.state.word}</h1>
         <input onChange={this.changeHandler} value={this.state.word} />
         <button>add item</button> */}
@@ -115,10 +155,47 @@ class List extends React.Component {
 class TodoItem extends React.Component {
   constructor() {
     super();
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.doneTodo = this.doneTodo.bind(this);
+  }
+
+  deleteTodo() {
+    if (this.props.detectDone) {
+      this.props.deleteTodo(this.props.donedes, this.props.detectDone);
+    } else {
+      this.props.deleteTodo(this.props.description);
+    }
+  }
+
+  doneTodo() {
+    this.props.doneTodo(this.props.description);
   }
 
   render() {
-    return <p>{this.props.description}</p>;
+    const timestamp = moment().format("LLLL, ZZ");
+
+    if (this.props.description) {
+      return (
+        <div>
+          <p style={{ display: "inline" }}>{this.props.description}</p>
+          <br />
+          <small>{timestamp}</small>
+          <button onClick={this.deleteTodo}>Del</button>
+          <button onClick={this.doneTodo}>Done</button>
+        </div>
+      );
+    } else if (this.props.donedes) {
+      return (
+        <div>
+          <p className="doneList" style={{ display: "inline" }}>
+            {this.props.donedes}
+          </p>
+          <br />
+          <small>{timestamp}</small>
+          <button onClick={this.deleteTodo}>Del</button>
+        </div>
+      );
+    }
   }
 }
 
